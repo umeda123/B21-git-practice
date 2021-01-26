d2b6d7d0286310c610af02f4abacac70879c17e2
umeda Davlatova <umeda.davlatova@gmail.com>
2021-01-26 14:24:56 -0500

    After few practice

--- src/main/java/FeatureACode.java
+++ src/main/java/FeatureACode.java
@@ -2,5 +2,7 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome day today  " );
+
+        System.out.println("Learning Git ");
     }
 }
