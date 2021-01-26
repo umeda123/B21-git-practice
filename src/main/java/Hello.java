4ce3a2e01557ac89f610a7320f4a88d622e7585a
umeda Davlatova <umeda.davlatova@gmail.com>
2021-01-26 15:17:18 -0500

    movie 1 branch updated in line 1 and 2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello Wold ,B21");
-        System.out.println("hello wald ");
+        System.out.println("Hello Wold ,B21 in ");
+        System.out.println("hello wald  hey ");
     }
 }
